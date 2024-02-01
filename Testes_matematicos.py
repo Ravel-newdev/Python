74e681b97ea9d5d66f55aefad5f5378eee18bd63
Ravel Costa <ravelcostadearaujo@gmail.com>
2024-01-31 22:24:23 -0300

    Adicionando arquivo de testes de lógica de calculo

---
+++ Testes_matematicos.py
@@ -0,0 +1,14 @@
+num = int(input("Informe um numero: "))
+
+ant = num - 1
+suc = num + 1
+
+print("O numero é: ", num,"\n O seu sucessor é o numero: ", suc,"\n O seu antecessor é o numero: ", ant," \n Calculo feito com sucesso")
+
+cal = int(input("Informe um numero: "))
+
+Dobro = cal * 2
+Triplo = cal * 3
+r_q = cal ** (1/2)
+r_q = round(r_q, 2)
+print("O Dobro do numero é: ", Dobro , "\n O triplo do numero é: ", Triplo, "\n A sua raíz quadrada: ", r_q)
