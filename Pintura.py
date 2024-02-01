ccf64ba21acf6408d941bbbd3171ff147be67687
Ravel Costa <ravelcostadearaujo@gmail.com>
2024-01-31 22:38:25 -0300

    Adicionando arquivo

---
+++ Pintura.py
@@ -0,0 +1,6 @@
+alt = float(input("Informe a altura da sua parede(se souber): "))
+larg = float(input("A largura também: "))
+
+tamanho = alt * larg
+tinta = tamanho / 2
+print("O tamanho dessa sua parede é de:", tamanho,"m2, vai gastar: ", tinta,"litros de tinta para pintar a parede")
